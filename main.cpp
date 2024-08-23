ab0c7f096400315214229c747bdede9706fc874b
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2024-08-23 12:40:13 -0400

    start adding user input code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <cstdlib>
+#include <cctype>
 #include "luhn.h"
 
 using namespace std;
 
 int main(void)
 {
+	ssize_t totalCharsInput, bufferLength = 0;
+	char **charsInput = NULL;
+
 	// Note: we are working with the numerical integer value
 	// of the chars, NOT the ASCII character representations
-	char ccNumToVerify[] = {4, 1, 4, 2,
+	/* char ccNumToVerify[] = {4, 1, 4, 2,
 				3, 8, 3, 3,
 				3, 5, 3, 2,
-				5, 1, 8, 9};
+				5, 1, 8, 9}; */
+
+	cout << "Enter number to test WITHOUT Luhn digit: ";
+	totalCharsInput = getline(charsInput, bufferLength, cin);
+	if(totalCharsInput == -1)
+		exit(EXIT_FAILURE);
+
+	//verify input
+	for(int i=0;i<totalCharsInput-1;i++)
+	{
+		
+	}
+
 
 	bool isLuhnValid;
 	int luhnDigit;
